9d46b544fe8d6dade2faee5001dd12a3504938db
Lais Aquino <laisaquino00@gmail.com>
2021-06-08 20:36:34 -0300

    Update lista_adicionar_remover.py

--- exercicios/lista_adicionar_remover.py
+++ exercicios/lista_adicionar_remover.py
@@ -1,9 +1,7 @@
 frutas=['maça','morango','melancia','mamão']
 print(frutas)
-
 remover=input('Qual fruta você gostaria de remover?')
 frutas.remove(remover)
-
 adicionar=input('Qual fruta você deseja acrescentar?')
 frutas.append(adicionar)
 print('Nova lista', frutas)
