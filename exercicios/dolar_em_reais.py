fca8356dfbbf6d1525df1f3b6a3967372fdcc076
Lais Aquino <laisaquino00@gmail.com>
2021-06-08 20:26:06 -0300

    Update dolar_em_reais.py

--- exercicios/dolar_em_reais.py
+++ exercicios/dolar_em_reais.py
@@ -1,4 +1,4 @@
-cotocap_dolar=float(input('Digie a cotação do dolar atual:'))
-converter=float(input('Qual é o valor a ser convertido?'))
-resultado=converter/0.25
+cotocap_dolar=float(input('Digite a cotação do dolar atual R$ :'))
+converter=float(input('Qual é o valor em dolar a ser convertido?'))
+resultado=converter*cotocap_dolar
 print('O valor de dolar em reais é:{}'.format(resultado))
