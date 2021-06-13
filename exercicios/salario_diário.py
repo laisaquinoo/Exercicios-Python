dd7ed6949b03d2b534855dabdefc41f7d1f16968
Lais Aquino <laisaquino00@gmail.com>
2021-06-13 18:41:45 -0300

    Update salario_diário.py

--- exercicios/salario_diário.py
+++ exercicios/salario_diário.py
@@ -1,5 +1,5 @@
 salario=float(input('digite o salario:'))
 diastrabalhado=int(input('digite os dias trabalhados:'))
 valor=salario/diastrabalhado
-print('o valor diário é:{}'.format(valor))
+print('o valor diário é: %.2f' %valor)
 
